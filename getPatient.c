65a0435edff3d4c7abb4d79f8362ab2d62478c62
lenacgs <lenacgs@gmail.com>
2017-10-18 00:02:45 +0100

    Nova consulta: ir buscar paciente à MQ, tempo de consulta, etc

---
+++ getPatient.c
@@ -0,0 +1,12 @@
+void getPatient() {
+
+	//ir buscar paciente à MQ
+
+	//registar log do paciente no sistema
+
+	wait(patient.duration); //confirmar se é mesmo para utilizar um wait()
+
+	//registar saida do paciente no sistema
+
+	//apagar paciente
+}
