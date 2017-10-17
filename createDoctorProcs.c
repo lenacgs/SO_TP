c3c36eb06553c15295c64cf557874cf019c0cc48
lenacgs <lenacgs@gmail.com>
2017-10-18 00:54:37 +0100

    "Madalena: small changes"

--- createDoctorProcs.c
+++ createDoctorProcs.c
@@ -1,7 +1,7 @@
 void createDoctorProcs() {
 	if (fork() == 0) {
 		printf("Doctor process with PID:%d created.\n", getpid());
-		goToWork();
+		DoctorgoToWork();
 		printf("Doctor process with PID: %d --- Shift finished.\n Doctor process leaving work.\n", getpid());
 		exit(0);
 	}
