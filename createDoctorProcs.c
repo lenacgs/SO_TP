cc17542b230fd9f18e7fa00cdfcdc7d9dfba9dbf
lenacgs <lenacgs@gmail.com>
2017-10-21 20:19:03 +0100

    no changes

--- createDoctorProcs.c
+++ createDoctorProcs.c
@@ -1,8 +1,10 @@
+#include "header.h"
+
 void createDoctorProcs() {
 	if (fork() == 0) {
 		printf("Doctor process with PID:%d created.\n", getpid());
-		DoctorgoToWork();
-		printf("Doctor process with PID: %d --- Shift finished.\n Doctor process leaving work.\n", getpid());
+		//DoctorgoToWork();
+		printf("Doctor process with PID: %d --- Shift finished.\nDoctor process leaving work.\n", getpid());
 		exit(0);
 	}
 }
