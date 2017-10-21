27b90e06dcc87b9b24f742522c07712425cc5712
lenacgs <lenacgs@gmail.com>
2017-10-21 20:19:45 +0100

    finished

--- readConfig.c
+++ readConfig.c
@@ -1,12 +1,18 @@
+#include "header.h"
+
 void readConfig() {
-	int val [10] = {NULL};
-	int i = 0;
+	int val[4], i=0;
 	FILE * file;
 	file = fopen("Config.txt", "r");
+
 	if (file) {
-		while (fscanf(file, "%d", val[i]) != EOF) {
-			//change variables to values read
-		}
+		while (fscanf(file, "%d", &val[i]) != EOF)
+			i++;
+
+		n_triage = val[0];
+		n_doctors = val[1];
+		shift_dur = val[2];
+		mq_max = val[3];
 	}
-	fclose(file);
+	fclose (file);
 }
